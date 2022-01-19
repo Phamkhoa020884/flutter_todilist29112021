fc5dafcfd814e895be60df875b18646225278623
khoa <hatgiong@gmail.com>
2022-01-19 17:40:40 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_todilist29112021/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_todilist29112021
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/features/post/detail/post_detail_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class PostDetailScreen extends StatefulWidget {
+  const PostDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  _PostDetailScreenState createState() => _PostDetailScreenState();
+}
+
+class _PostDetailScreenState extends State<PostDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
